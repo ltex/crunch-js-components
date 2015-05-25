4c1fb339ef6806f9fb2f53355de539419144cf70
Enrique Alcantara <enriquealc27@gmail.com>
2015-05-25 11:28:34 -0400

    extend filter builder example to demonstrate how to save, load and edit a filter

--- examples/filter-builder-ctrl.js
+++ examples/filter-builder-ctrl.js
@@ -1,16 +1,5 @@
 'use strict';
 
-FilterBuilderCtrl.$inject = [
-    '$scope'
-    , 'currentDataset'
-    , 'FilterBuilder'
-    , 'iFetchHierarchicalVariables'
-    , 'createHVL'
-    , 'signin'
-    //, 'ShareFilter'
-]
-
-
 app.factory('createHVL', function(HierarchicalVariablesList) {
     'use strict'
 
@@ -29,57 +18,85 @@ app.factory('createHVL', function(HierarchicalVariablesList) {
     }
 })
 
-function FilterBuilderCtrl($scope, currentDataset, FilterBuilder, iFetchHierarchicalVariables, createHVL, signin){//, ShareFilter) {
+
+app.factory('persistFilter', ['filterCompiler', function(filterCompiler) {
+
+    return function(dataset, name, expressions, junctions, op) {
+        //op can be either update or save
+        return dataset.urls.filters[op || 'save']({
+            data : {
+                name : name,
+                expression : filterCompiler(expressions, junctions),
+                is_public : true
+            }
+        })
+    }
+}])
+
+app.factory('loadFilter', ['Shoji', 'filterDecompiler', function(Shoji, filterDecompiler) {
+    return function(filterId, dataset) {
+        return Shoji(filterId).map().then(function(filter) {
+            return filterDecompiler(filter, dataset)
+        })
+    }
+}])
+
+function FilterBuilderCtrl($scope, currentDataset, FilterBuilder, iFetchHierarchicalVariables, createHVL, signin, persistFilter, loadFilter){
     var dataset
         , filterBuilder
-        , shareFilter
+        ;
 
-    this.init = function(){
+    signin
+    .apply(signin, secrets.credentials)
+    .then(function() {
+        return iFetchHierarchicalVariables({
+            datasetId : secrets.dataset
+        })
+    })
+    .then(function(variables) {
+        $scope.hierarchicalVariablesList = createHVL(variables)
 
-        signin.apply(signin, secrets.credentials)
-            .then(function() {
-                return iFetchHierarchicalVariables({
-                    datasetId : secrets.dataset
-                })
+        currentDataset.fetch()
+        .then(function (ds) {
+            dataset = ds
+            filterBuilder = $scope.filterBuilder = FilterBuilder.create({
+                dataset: dataset
             })
-            .then(function(variables) {
-                $scope.hierarchicalVariablesList = createHVL(variables)
+        })
+    })
 
-                currentDataset.fetch()
-                    .then(function (ds) {
-                        dataset = ds
+    $scope.saveFilter = function() {
+        var filter = filterBuilder.getFilter()
+            ;
 
-                        filterBuilder = $scope.filterBuilder = FilterBuilder.create({
-                            dataset: dataset
-                            // use iFetchDatasetFilters to get a filter and
-                            // add to a filter argument here
-                            //, filter: filter
-                        })
-
-                        /*
-                         //this creates a sharing togle for the filter
-                         shareFilter = $scope.shareFilter = ShareFilter.create({
-                         dataset : dataset
-                         , share : false
-                         })*/
-                    })
-
+        persistFilter(dataset, filter.name, filter.expressions, filter.junctions)
+            .then(function(newFilter) {
+                console.log('Filter created ', newFilter.self)
             })
+            .catch(function() {
+                console.log('Filter could not be saved')
+            })
+    }
 
-        /*
-
-         Here is an example of how to interact with the service bus to implement your filter (create or edit
-         $scope.$on('filter.applied', function() {
-         })
-
-         $scope.$on('filter.edited', function($e, evt) {
-         bus.send({
-         command: 'applyFilter'
-         , id: dataset.self
-         , filterId: evt.filterId
-         })
-         })*/
+    $scope.loadFilter = function() {
+        loadFilter($scope.filterId, dataset).then(function(filter) {
+            filterBuilder = $scope.filterBuilder = FilterBuilder.create({
+                dataset : dataset
+                , filter : filter
+            })
+        })
     }
 }
+
+FilterBuilderCtrl.$inject = [
+    '$scope'
+    , 'currentDataset'
+    , 'FilterBuilder'
+    , 'iFetchHierarchicalVariables'
+    , 'createHVL'
+    , 'signin'
+    , 'persistFilter'
+    , 'loadFilter'
+]
 
 app.controller('FilterBuilderCtrl', FilterBuilderCtrl)
